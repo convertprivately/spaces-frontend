b3772600e5fc9db07ea9c6228d41a888fba152f4
Egemen Göl <egemengol@gmail.com>
2023-11-02 01:32:07 +0300

    modify ssl env var parsing

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
 export const HOST = process.env.NEXT_PUBLIC_HOST || "localhost:8000";
-export const SSL = process.env.NEXT_PUBLIC_SSL || false;
+export const SSL = process.env.NEXT_PUBLIC_SSL === 'true' || false;
 export const API_URL = `http${SSL ? "s" : ""}://${HOST}`;
 export const WS_SPACES_URL = `ws${SSL ? "s" : ""}://${HOST}/ws/spaces`;
 
