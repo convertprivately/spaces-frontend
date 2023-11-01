e37034dec87373a4fe3753e874328421d4dadd9c
Egemen Göl <egemengol@gmail.com>
2023-11-02 01:28:33 +0300

    prepend NEXT_PUBLIC_ to envs

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
-export const HOST = process.env.HOST || "localhost:8000";
-export const SSL = process.env.SSL || false;
+export const HOST = process.env.NEXT_PUBLIC_HOST || "localhost:8000";
+export const SSL = process.env.NEXT_PUBLIC_SSL || false;
 export const API_URL = `http${SSL ? "s" : ""}://${HOST}`;
 export const WS_SPACES_URL = `ws${SSL ? "s" : ""}://${HOST}/ws/spaces`;
 
